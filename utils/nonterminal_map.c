a36f226994c96bcd686d730ecd398336ca9e9ade
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2020-10-18 19:16:11 +0530

    Updated nonterminal and terminal maps

--- utils/nonterminal_map.c
+++ utils/nonterminal_map.c
@@ -14,235 +14,111 @@ hash_map *generate_nt_map(int map_size)
 
   temp = calloc(1, sizeof(int));
   *temp = 0;
-  insert_into_map(map, "program", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 1;
-  insert_into_map(map, "moduleDeclarations", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 2;
-  insert_into_map(map, "otherModules", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 3;
-  insert_into_map(map, "driverModule", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 4;
-  insert_into_map(map, "otherModule", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 5;
-  insert_into_map(map, "moduleDeclaration", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 6;
   insert_into_map(map, "module", temp);
 
   temp = calloc(1, sizeof(int));
+  *temp = 1;
+  insert_into_map(map, "decStmtGrp", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 2;
+  insert_into_map(map, "assignStmtGrp", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 3;
+  insert_into_map(map, "decStmt", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 4;
+  insert_into_map(map, "assignStmt", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 5;
+  insert_into_map(map, "primitiveDecStmt", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 6;
+  insert_into_map(map, "jagArrDecStmt", temp);
+
+  temp = calloc(1, sizeof(int));
   *temp = 7;
-  insert_into_map(map, "moduleDef", temp);
+  insert_into_map(map, "primitiveDataType", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 8;
-  insert_into_map(map, "input_plist", temp);
+  insert_into_map(map, "multipleIdList", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 9;
-  insert_into_map(map, "ret", temp);
+  insert_into_map(map, "jagArrRange", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 10;
-  insert_into_map(map, "output_plist", temp);
+  insert_into_map(map, "jagArrInit", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 11;
-  insert_into_map(map, "dataType", temp);
+  insert_into_map(map, "jagArrDimDec", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 12;
-  insert_into_map(map, "input_plist2", temp);
+  insert_into_map(map, "jagArrValues", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 13;
-  insert_into_map(map, "type", temp);
+  insert_into_map(map, "numList", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 14;
-  insert_into_map(map, "output_plist2", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 15;
-  insert_into_map(map, "range_arrays", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 16;
-  insert_into_map(map, "index_nt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 17;
-  insert_into_map(map, "statements", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 18;
-  insert_into_map(map, "statement", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 19;
-  insert_into_map(map, "ioStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 20;
-  insert_into_map(map, "simpleStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 21;
-  insert_into_map(map, "declareStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 22;
-  insert_into_map(map, "condionalStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 23;
-  insert_into_map(map, "iterativeStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 24;
-  insert_into_map(map, "var", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 25;
-  insert_into_map(map, "boolConstt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 26;
-  insert_into_map(map, "var_id_num", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 27;
-  insert_into_map(map, "whichId", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 28;
-  insert_into_map(map, "assignmentStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 29;
-  insert_into_map(map, "moduleReuseStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 30;
-  insert_into_map(map, "whichStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 31;
-  insert_into_map(map, "lvalueIDStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 32;
-  insert_into_map(map, "lvalueARRStmt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 33;
-  insert_into_map(map, "expression", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 34;
-  insert_into_map(map, "optional", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 35;
   insert_into_map(map, "idList", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 36;
-  insert_into_map(map, "idList2", temp);
+  *temp = 15;
+  insert_into_map(map, "arrayRangeList", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 37;
-  insert_into_map(map, "arithmeticOrBooleanExpr", temp);
+  *temp = 16;
+  insert_into_map(map, "arrayRange", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 38;
-  insert_into_map(map, "unary_nt", temp);
+  *temp = 17;
+  insert_into_map(map, "numOrId", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 39;
-  insert_into_map(map, "unary_op", temp);
+  *temp = 18;
+  insert_into_map(map, "numOrIdList", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 40;
-  insert_into_map(map, "new_NT", temp);
+  *temp = 19;
+  insert_into_map(map, "jagArrElement", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 41;
-  insert_into_map(map, "arithmeticExpr", temp);
+  *temp = 20;
+  insert_into_map(map, "arrayElement", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 42;
-  insert_into_map(map, "AnyTerm", temp);
+  *temp = 21;
+  insert_into_map(map, "operand", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 43;
-  insert_into_map(map, "arithmeticOrBooleanExpr2", temp);
+  *temp = 22;
+  insert_into_map(map, "assignLhs", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 44;
-  insert_into_map(map, "logicalOp", temp);
+  *temp = 23;
+  insert_into_map(map, "arithExpression", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 45;
-  insert_into_map(map, "AnyTerm2", temp);
+  *temp = 24;
+  insert_into_map(map, "boolExpression", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 46;
-  insert_into_map(map, "relationalOp", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 47;
+  *temp = 25;
   insert_into_map(map, "term", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 48;
-  insert_into_map(map, "arithmeticExpr2", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 49;
-  insert_into_map(map, "op1", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 50;
-  insert_into_map(map, "factor", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 51;
-  insert_into_map(map, "term2", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 52;
-  insert_into_map(map, "op2", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 53;
-  insert_into_map(map, "caseStmts", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 54;
-  insert_into_map(map, "default_nt", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 55;
-  insert_into_map(map, "value", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 56;
-  insert_into_map(map, "caseStmts2", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 57;
-  insert_into_map(map, "range", temp);
+  *temp = 26;
+  insert_into_map(map, "andExpression", temp);
 
   temp = NULL;
   return map;
--- utils/terminal_map.c
+++ utils/terminal_map.c
@@ -18,11 +18,11 @@ hash_map *generate_t_map(int map_size)
 
   temp = calloc(1, sizeof(int));
   *temp = 1;
-  insert_into_map(map, "MODULE", temp);
+  insert_into_map(map, "ID", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 2;
-  insert_into_map(map, "ID", temp);
+  insert_into_map(map, "COLON", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 3;
@@ -30,219 +30,103 @@ hash_map *generate_t_map(int map_size)
 
   temp = calloc(1, sizeof(int));
   *temp = 4;
-  insert_into_map(map, "DRIVERDEF", temp);
+  insert_into_map(map, "LIST", temp);
 
   temp = calloc(1, sizeof(int));
   *temp = 5;
-  insert_into_map(map, "DRIVER", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 6;
-  insert_into_map(map, "PROGRAM", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 7;
-  insert_into_map(map, "DRIVERENDDEF", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 8;
-  insert_into_map(map, "DEF", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 9;
-  insert_into_map(map, "ENDDEF", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 10;
-  insert_into_map(map, "TAKES", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 11;
-  insert_into_map(map, "INPUT", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 12;
-  insert_into_map(map, "SQBO", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 13;
-  insert_into_map(map, "SQBC", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 14;
-  insert_into_map(map, "RETURNS", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 15;
-  insert_into_map(map, "COLON", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 16;
-  insert_into_map(map, "COMMA", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 17;
-  insert_into_map(map, "INTEGER", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 18;
-  insert_into_map(map, "REAL", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 19;
-  insert_into_map(map, "BOOLEAN", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 20;
-  insert_into_map(map, "ARRAY", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 21;
   insert_into_map(map, "OF", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 22;
-  insert_into_map(map, "RANGEOP", temp);
+  *temp = 6;
+  insert_into_map(map, "VARIABLES", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 23;
-  insert_into_map(map, "START", temp);
+  *temp = 7;
+  insert_into_map(map, "JAGGED", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 24;
-  insert_into_map(map, "END", temp);
+  *temp = 8;
+  insert_into_map(map, "ARRAY", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 25;
-  insert_into_map(map, "BO", temp);
+  *temp = 9;
+  insert_into_map(map, "INTEGER", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 26;
-  insert_into_map(map, "BC", temp);
+  *temp = 10;
+  insert_into_map(map, "R1", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 27;
-  insert_into_map(map, "GET_VALUE", temp);
+  *temp = 11;
+  insert_into_map(map, "SQ_OPEN", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 28;
-  insert_into_map(map, "PRINT", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 29;
-  insert_into_map(map, "TRUE", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 30;
-  insert_into_map(map, "FALSE", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 31;
+  *temp = 12;
   insert_into_map(map, "NUM", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 32;
-  insert_into_map(map, "RNUM", temp);
+  *temp = 13;
+  insert_into_map(map, "SQ_CLOSE", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 33;
-  insert_into_map(map, "ASSIGNOP", temp);
+  *temp = 14;
+  insert_into_map(map, "SIZE", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 34;
-  insert_into_map(map, "USE", temp);
+  *temp = 15;
+  insert_into_map(map, "VALUES", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 35;
-  insert_into_map(map, "WITH", temp);
+  *temp = 16;
+  insert_into_map(map, "CUR_OPEN", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 36;
-  insert_into_map(map, "PARAMETERS", temp);
+  *temp = 17;
+  insert_into_map(map, "CUR_CLOSE", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 37;
-  insert_into_map(map, "PLUS", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 38;
-  insert_into_map(map, "MINUS", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 39;
-  insert_into_map(map, "MUL", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 40;
-  insert_into_map(map, "DIV", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 41;
-  insert_into_map(map, "AND", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 42;
-  insert_into_map(map, "OR", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 43;
-  insert_into_map(map, "LT", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 44;
-  insert_into_map(map, "LE", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 45;
-  insert_into_map(map, "GT", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 46;
-  insert_into_map(map, "GE", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 47;
-  insert_into_map(map, "EQ", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 48;
-  insert_into_map(map, "NE", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 49;
-  insert_into_map(map, "SWITCH", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 50;
-  insert_into_map(map, "CASE", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 51;
-  insert_into_map(map, "BREAK", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 52;
-  insert_into_map(map, "DEFAULT", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 53;
-  insert_into_map(map, "FOR", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 54;
-  insert_into_map(map, "IN", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 55;
-  insert_into_map(map, "WHILE", temp);
-
-  temp = calloc(1, sizeof(int));
-  *temp = 56;
+  *temp = 18;
   insert_into_map(map, "EPS", temp);
 
   temp = calloc(1, sizeof(int));
-  *temp = 57;
-  insert_into_map(map, "DOLLAR", temp);
+  *temp = 19;
+  insert_into_map(map, "REAL", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 20;
+  insert_into_map(map, "BOOLEAN", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 21;
+  insert_into_map(map, "ELIP", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 22;
+  insert_into_map(map, "EQUALS", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 23;
+  insert_into_map(map, "PLUS", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 24;
+  insert_into_map(map, "MINUS", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 25;
+  insert_into_map(map, "MUL", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 26;
+  insert_into_map(map, "DIV", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 27;
+  insert_into_map(map, "OR", temp);
+
+  temp = calloc(1, sizeof(int));
+  *temp = 28;
+  insert_into_map(map, "AND", temp);
 
   temp = NULL;
   return map;
