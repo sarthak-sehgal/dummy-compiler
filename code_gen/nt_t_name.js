5484b8b4c400b825eb8cbe37e74a777b76f4659e
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2020-10-18 19:20:18 +0530

    Added code generator for term & nonterm name.c files

---
+++ code_gen/nt_t_name.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const data = require("./data.json");
+const dataKey = "nonterminals";
+const text = `case _COUNTER_:\nstrcpy(name, _NAME_);\nbreak;\n`;
+const file_name = `${dataKey}_name_c.txt`;
+
+let res = "";
+let count = 0;
+
+if (data[dataKey] == undefined) {
+  throw new Error(`Data for ${dataKey} not found.`);
+}
+
+for (const key of data[dataKey]) {
+  const str = text.replace("_COUNTER_", count).replace("_NAME_", `"${key}"`);
+  res += str;
+  count++;
+}
+
+fs.writeFile(path.resolve(__dirname, file_name), res, (err) => {
+  if (err) throw err;
+  console.log(`${file_name} saved.`);
+});
